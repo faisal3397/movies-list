765ac24609994a50b5c7b2e13d9e6e8369bbfa5d
faisal3397 <faisal.alowaisi.97@gmail.com>
2019-06-25 13:30:25 +0300

    change the app layout when the language is arabic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2 } from '@angular/core';
 import { Movie } from './movies/movie.model';
 import {TranslateService} from '@ngx-translate/core';
 
@@ -10,11 +10,16 @@ import {TranslateService} from '@ngx-translate/core';
 export class AppComponent {
   title = 'movies-list';
   navigateTo = 'movies';
-  constructor(private translate: TranslateService) {
+  constructor(private translate: TranslateService, private renderer: Renderer2) {
     translate.setDefaultLang('en');
   }
   
   useLanguage(language: string) {
+    if( language == 'en') {
+      this.renderer.setAttribute(document.body, 'dir', 'ltr');
+    } else {
+      this.renderer.setAttribute(document.body, 'dir', 'rtl');
+    }
     this.translate.use(language);
   }
 
